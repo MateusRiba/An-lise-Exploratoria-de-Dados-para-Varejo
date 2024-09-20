797b50bd8dd49a05bb3e999dc0f959f0f79cca43
MateusRiba <Mateusbaaca@gmail.com>
2024-09-20 17:35:51 -0300

    Todas as perguntas respondidas

--- Main.py
+++ Main.py
@@ -123,8 +123,98 @@ df['Ano'] = df['Data_Pedido'].dt.year #Adicionando uma nova coluna
 
 print(df)
 
-Vendas_Por_Segmento_e_Ano = df.groupby(['Segmento', 'Ano'])['Valor_Venda'].sum()
+Vendas_Por_Segmento_e_Ano = df.groupby(['Ano', 'Segmento'])['Valor_Venda'].sum()
 print(f'\n Aqui estão as Vendas por segmento e por ano: \n{Vendas_Por_Segmento_e_Ano}')
 
 print('\n-----------------------------------------------------------Pergunta 7---------------------------------------------------------------------------------\n')
 
+#PERGUNTA 7: Os gestores da empresa estão considerando conceder diferentes faixas de descontos e gostariam de fazer uma simulação com base na regra abaixo:
+#- Se o Valor_Venda for maior que 1000 recebe 15% de desconto.
+#- Se o Valor_Venda for menor que 1000 recebe 10% de desconto.
+
+### Quantas Vendas Receberiam 15% de Desconto?
+
+QuantidadeQueRecebe15PorCento =  df[df['Valor_Venda'] > 1000].shape[0]
+print(f'\n A quantidade de vendas que receberiam 15% de desconto são: {QuantidadeQueRecebe15PorCento}')
+
+#OBS: Outro metodo de fazer:
+
+# Cria uma nova coluna de acordo com a regra definida acima  ------> Dessa maneira uma nova coluna com os valores de desconto são criadas,
+#mais complexo para responder a pergunta porem util em outros casos
+#df['Desconto'] = np.where(df['Valor_Venda'] > 1000, 0.15, 0.10)
+
+print('\n-----------------------------------------------------------Pergunta 8---------------------------------------------------------------------------------\n')
+
+#PERGUNTA 8: Considere Que a Empresa Decida Conceder o Desconto de 15% do Item Anterior. Qual Seria a Média do Valor de Venda Antes e Depois do Desconto?
+
+df['Valor_Venda_Com_Desconto_15%'] = df['Valor_Venda'].where(df['Valor_Venda'] <= 1000, df['Valor_Venda'] * 0.85) #uma maneira
+#df['Valor_Venda_Com_Desconto_15%'] = df['Valor_Venda'].apply(lambda x: x * 0.85 if x > 1000 else x) #outra maneira
+
+#Descontos aplicados
+print(df[df['Valor_Venda'] > 1000])
+
+#Conseguir a media entre as duas colunas
+
+media = df['Valor_Venda'].mean()
+media2 = df['Valor_Venda_Com_Desconto_15%'].mean()
+
+print(f'\nA média dos valores venda sem desconto é: {media}\n A média dos valores venda com desconto é {media2}')
+
+print('\n-----------------------------------------------------------Pergunta 9---------------------------------------------------------------------------------\n')
+
+#PERGUNTA 9: Qual o Média de Vendas Por Segmento, Por Ano e Por Mês? (Demonstre o resultado através de gráfico de linha.)
+
+df['Mês'] = df['Data_Pedido'].dt.month
+
+Média_Segmento = df.groupby(['Segmento','Ano','Mês'])['Valor_Venda'].mean().reset_index()
+
+print(Média_Segmento)
+
+#Cria uma paleta de cores personalizada
+paleta = sns.color_palette("husl", len(Média_Segmento['Segmento'].unique()))
+
+#Cria o grid de gráficos com base no ano
+g = sns.FacetGrid(Média_Segmento, col="Ano", hue="Segmento", palette=paleta, col_wrap=2, height=5, aspect=1.5)
+
+#Adiciona os gráficos de linha em cada subplot
+g.map(sns.lineplot, 'Mês', 'Valor_Venda', marker="o")
+
+#Adicionar título geral
+g.figure.suptitle('Média de Vendas por Segmento, Ano e Mês', fontsize=16)
+
+#Ajusta os rótulos dos eixos e o layout
+g.set_axis_labels("Mês", "Média do Valor de Vendas")
+g.add_legend(title="Segmento")
+
+#Adiciona o grid
+g.set(xticks=range(1, 13))  # Colocar todos os meses no eixo x (1-12)
+plt.subplots_adjust(top=0.9)  # Ajustar a posição do título geral
+
+#plt.show()
+
+print('\n-----------------------------------------------------------Pergunta 10---------------------------------------------------------------------------------\n')
+
+#PERGUNTA 10: Qual o Total de Vendas Por Categoria e SubCategoria, Considerando Somente as Top 12 SubCategorias? (Demonstre tudo através de um único gráfico.)
+
+top12Subcategorias = df.groupby(['Categoria','SubCategoria']).sum(numeric_only = True).sort_values('Valor_Venda',ascending = False).head(12).reset_index()
+total_vendas_categoria = df.groupby('Categoria')['Valor_Venda'].sum().reset_index()
+
+print(top12Subcategorias)
+
+plt.figure(figsize=(14, 8))
+sns.barplot(x='Valor_Venda', y='SubCategoria', hue='Categoria', data=top12Subcategorias, palette='Set2')
+
+plt.title('Total de Vendas por Categoria e SubCategoria (Top 12 SubCategorias)')
+plt.xlabel('Total de Vendas')
+plt.ylabel('SubCategoria')
+
+for i, categoria in enumerate(total_vendas_categoria['Categoria']):
+    total_vendas = total_vendas_categoria[total_vendas_categoria['Categoria'] == categoria]['Valor_Venda'].values[0]
+    plt.text(top12Subcategorias['Valor_Venda'].max() * 1.05, i * 4,  
+             f'Total {categoria}: R$ {total_vendas:,.2f}', 
+             fontsize=12, color='black')
+
+
+plt.tight_layout()
+
+plt.show()
